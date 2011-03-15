2210d562839f5dabd2056d03a20e38f9862af87e
Jeremy Kelley <jeremy@collectivelabs.com>
2011-03-15 16:19:38 -0500

    survey revisions

--- views/survey_definitions.py
+++ views/survey_definitions.py
@@ -11,10 +11,63 @@ registration = models.Survey(
             key='attending',
             ),
         dict(
+            title="What's your zip/postal code?",
+            formtype='text',
+            key='zip',
+            ),
+        dict(
+            title="What's your email address?",
+            formtype='text',
+            key='email',
+            ),
+        dict(
+            title='What size shirt?',
+            formtype='radio',
+            options=[('small', 'Small'), ('med','Medium'),
+                ('large','Large'), ('xlarge', 'X-Large'),
+                ('xxl', 'XX-Large'),
+                ],
+            key='shirt',
+            ),
+        ]
+    )
+
+
+optional = models.Survey(
+    key = 'pytx11opt',
+    title = 'PyTX11 Survey',
+    questions = [
+        dict(
             title='How experienced with Python are you?',
             formtype='radio',
             options=[('teacher', 'Teacher'), ('learner','Learner')],
             key='pyexp',
+            ),
+        dict(
+            title=':15 ',
+            formtype='radio',
+            options=[('yes','Yes'), ('maybe', 'Maybe')],
+            key='attending',
+            ),
+        dict(
+            title='How experienced with Python are you?',
+            formtype='radio',
+            options=[('teacher', 'Teacher'), ('learner','Learner')],
+            key='pyexp',
+            ),
+        dict(
+            title="What's your email address?",
+            formtype='text',
+            key='email',
+            ),
+        dict(
+            title='What size shirt?',
+            formtype='radio',
+            options=[('small', 'Small'), ('med','Medium'),
+                ('large','Large'), ('xlarge', 'X-Large'),
+                ('xxl', 'XX-Large'),
+                ],
+            key='shirt',
             ),
         ]
     )
