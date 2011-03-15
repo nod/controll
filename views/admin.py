441e740c93d36c0c84b2c9b565849cd78d8e2866
Jeremy Kelley <jeremy@collectivelabs.com>
2011-03-15 17:50:03 -0500

    shirts

--- views/admin.py
+++ views/admin.py
@@ -2,13 +2,13 @@ import tornado.web
 
 from . import route, BaseHandler, models
 
+import json
 
 @route('/admin/?', name="admin")
 class Admin(BaseHandler):
 
     @tornado.web.authenticated
     def get(self):
-
         admins = [
             'twit:nod', 'twit:joshmarshall',
             'twit:bradevans137',
@@ -17,7 +17,13 @@ class Admin(BaseHandler):
             self.render('oops.html', txt='bad dog. no donut.')
             return
         attendees = models.User.find()
-        self.render('admin.html', attendees=attendees)
+
+        shirts = {}
+        for a in attendees:
+            s = a.survey_results('pytx11reg')
+            shirts[s.answers['shirt']] = shirts.get(s.answers['shirt'],0) + 1
+
+        self.render('admin.html', attendees=attendees, shirts=shirts)
 
 
 @route('/admin/user/([:a-z0-9\-_]+)', name="admin-user")
@@ -26,6 +32,12 @@ class Login(BaseHandler):
     def get(self, userkey):
 
         user = models.User.search(key=userkey).first()
+
+        if self.get_argument('json',False):
+            self.write(json.dumps(
+                [s.answers for s in models.SurveyResponse.search(user=user) ]
+                ))
+            return
 
         self.render('admin_user.html',
             user=models.User.search(key=userkey).first(),
