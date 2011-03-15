a5f50cd32e34594a33d4fc725f9017b5c1b1b6c1
Jeremy Kelley <jeremy@collectivelabs.com>
2011-03-15 15:37:51 -0500

    fixing bugs

--- views/__init__.py
+++ views/__init__.py
@@ -5,12 +5,13 @@ import sys
 import os.path
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import models
+from base import BaseHandler
+
 import user
 import auth
 import index
+import survey
 
-from base import BaseHandler
-from survey import SurveyHandler
 
 # must come last, after all views are imported.
 routes = route.get_routes()
