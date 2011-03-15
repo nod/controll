eea4d146308d393721c1c8dfcb4b229e2c0c973d
Jeremy Kelley <jeremy@collectivelabs.com>
2011-03-15 12:33:30 -0500

    initial definition of models

---
+++ models/__init__.py
@@ -0,0 +1,50 @@
+from mogo import Model, Field, connect, ReferenceField
+
+
+class User(Model):
+
+    key = Field()
+    name = Field()
+    created_at = Field()
+    access_token = Field()
+    caste = Field()
+
+
+class Event(Model):
+
+    title = Field()
+    start_at = Field()
+    descr = Field()
+
+
+class SessionLocation(Model):
+
+    name = Field()
+    descr = Field()
+
+
+class Session(Model):
+
+    title = Field()
+    speaker = ReferenceField(SessionLocation)
+    descr = Field()
+    tags = Field()
+    start_at = Field()
+    duration = Field()
+    location = ReferenceField(SessionLocation)
+
+
+class Survey(Model):
+
+    key = Field()
+    title = Field()
+    questions = Field()
+
+
+class SurveyResponse(Model):
+
+    survey = ReferenceField(Survey)
+    user = ReferenceField(User)
+    answers = Field()
+
+
